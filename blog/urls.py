ed8d19d8c5b3597026368d876535e650954d37a2
jegedenifemi <jegedenifemi24@gmail.com>
2022-10-15 18:12:04 +0100

    first template

--- blog/urls.py
+++ blog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path('posts/', include('post.urls')),
+    path('', include('post.urls')),
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 if settings.DEBUG == False:
