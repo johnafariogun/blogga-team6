d315da7a2c1e98f8fc36427b0928b7cb07ecf9b4
John Tolulope Afariogun <afariogun.john@yahoo.com>
2022-10-15 13:12:00 +0100

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('posts/', include('post.urls')),
 
 ]+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG == FALSE:
+if settings.DEBUG == False:
     handler404 = "accounts.views.error_404_view"
     handler500 = "accounts.views.error_500_view"
