9a6ac7851526597d5350cb1873d29e652393ad8c
johnafariogun <afariogun.john@yahoo.com>
2022-10-21 13:17:40 +0100

    pushing urls

--- post/urls.py
+++ post/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     path('post/<slug:slug>/update/', views.update, name ='post_update'),
     path('category/<category>', views.CategoryListView.as_view(), name = 'post_category'),
     path('bookmarks/', views.bookmarks, name = 'bookmarks'),
+    path('archive/', views.archive_view, name='archive'),
+    path('our-team/', views.team_view, name='our-team'),
+    path('privacy-policy/', views.privacy_view, name ='privacy-policy'),
+    path('profile/', views.profile_view, name='profile'),
+    path('terms/',views.terms_view, name='terms')
     path('content/', views.contents),
     
 ]
