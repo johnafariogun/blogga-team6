eb949d6d359132cb7f474d3e307b8a3f9d7e94c4
johnafariogun <afariogun.john@yahoo.com>
2022-10-28 23:20:00 +0100

    battling

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import dj_database_url
 from pathlib import Path
 import django_heroku
@@ -17,12 +17,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, '.env'))
+# load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = '73siuk@n$yr&)_kg-bnu_t)ed=+q)*n7iq3s-s*snqrr3h9h!&'
+#  os.getenv("DJANGO_SECRET_KEY")
 #  config('DJANGO_SECRET_KEY')
 
 
@@ -93,7 +94,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'postgresql://postgres:63aXtIZo1cPMZMLpk569@containers-us-west-96.railway.app:6686/railway'
 DATABASES ={
     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
@@ -154,9 +156,11 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_USER = 'afariogun.john2002@gmail.com'
+EMAIL_HOST_PASSWORD= 'civgsenofnujoyiq'
 # os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_USER")
 # config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = '465'
 
