3d5712598ba4aa1ad875f1291534ee484959b63a
johnafariogun <afariogun.john@yahoo.com>
2022-10-28 21:09:59 +0100

    hosting

--- blog/settings.py
+++ blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from decouple import config 
+from dotenv import load_dotenv
 import dj_database_url
 from pathlib import Path
 import django_heroku
@@ -17,12 +17,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '73siuk@n$yr&)_kg-bnu_t)ed=+q)*n7iq3s-s*snqrr3h9h!&'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 #  config('DJANGO_SECRET_KEY')
 
 
@@ -154,9 +154,9 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'afariogun.john2002@gmail.com'
-# config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = 'lcbyxnnfocuqpwoc'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_USER")
 # config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = '465'
 
